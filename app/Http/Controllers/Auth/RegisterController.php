42b1ccaae648bc6ac39656af267ec7199bc5c032
Spencer Cheng <spencercheng@Spencers-MacBook-Pro.local>
2018-04-13 09:15:09 -0500

    set referral code as nullable

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -52,7 +52,7 @@ class RegisterController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
-            'referral'=>'string|exists:users,affiliate_id'
+            'referral'=>'nullable|string|exists:users,affiliate_id'
         ]);
     }
 
